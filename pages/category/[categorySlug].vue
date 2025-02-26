de7020fcb039481b5c8d45385e2ed11880e7a82e
Azamat <azamatrozumow@gmail.com>
2025-02-26 17:42:20 +0500

    [categorySlug] and [slug] added

--- pages/category/[categorySlug].vue
+++ pages/category/[categorySlug].vue
@@ -1,5 +1,150 @@
+<script setup lang="ts">
+import ProductGrid from '~/components/Card/ProductGrid.vue';
+import Container from '~/components/Container.vue';
+import ProductFilter from '~/components/Filter/ProductFilter.vue';
+import image1 from "@/assets/images/image1.png"
+import image2 from "@/assets/images/image2.png"
+import image3 from "@/assets/images/image3.png"
+import image4 from "@/assets/images/image4.png"
+
+import type { IProduct } from "@/types"
+const fetchFilter = (filters: { order: string; brands: string[]; searchQuery: string; }) => {
+    console.log('ff', filters);
+
+}
+const onClear = () => {
+    console.log('ff');
+
+}
+
+
+const initialProducts = ref<IProduct[]>([
+    {
+        id: 9,
+        name: "Французские духи Chanel №5 (50мл)",
+        price: 3000,
+        imageUrl: image1,
+    },
+    {
+        id: 10,
+        name: "Ювелирный набор - Серебряные украшения",
+        price: 3500,
+        imageUrl: image2,
+    },
+    {
+        id: 11,
+        name: "Набор сладостей Ferrero Rocher (200г)",
+        price: 1100,
+        imageUrl: image3,
+    },
+    {
+        id: 12,
+        name: "Косметический набор L’Oreal",
+        price: 1800,
+        imageUrl: image4,
+    },
+    {
+        id: 9,
+        name: "Французские духи Chanel №5 (50мл)",
+        price: 3000,
+        imageUrl: image1,
+    },
+    {
+        id: 10,
+        name: "Ювелирный набор - Серебряные украшения",
+        price: 3500,
+        imageUrl: image2,
+    },
+    {
+        id: 11,
+        name: "Набор сладостей Ferrero Rocher (200г)",
+        price: 1100,
+        imageUrl: image3,
+    },
+    {
+        id: 12,
+        name: "Косметический набор L’Oreal",
+        price: 1800,
+        imageUrl: image4,
+    },
+    {
+        id: 9,
+        name: "Французские духи Chanel №5 (50мл)",
+        price: 3000,
+        imageUrl: image1,
+    },
+    {
+        id: 10,
+        name: "Ювелирный набор - Серебряные украшения",
+        price: 3500,
+        imageUrl: image2,
+    },
+    {
+        id: 11,
+        name: "Набор сладостей Ferrero Rocher (200г)",
+        price: 1100,
+        imageUrl: image3,
+    },
+    {
+        id: 12,
+        name: "Косметический набор L’Oreal",
+        price: 1800,
+        imageUrl: image4,
+    },
+    {
+        id: 9,
+        name: "Французские духи Chanel №5 (50мл)",
+        price: 3000,
+        imageUrl: image1,
+    },
+    {
+        id: 10,
+        name: "Ювелирный набор - Серебряные украшения",
+        price: 3500,
+        imageUrl: image2,
+    },
+    {
+        id: 11,
+        name: "Набор сладостей Ferrero Rocher (200г)",
+        price: 1100,
+        imageUrl: image3,
+    },
+    {
+        id: 12,
+        name: "Косметический набор L’Oreal",
+        price: 1800,
+        imageUrl: image4,
+    },
+]);
+
+
+// Tasodifiy tartiblash funksiyasi
+const shuffleArray = (array: IProduct[]) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+};
+
+// Ref bilan popularProducts ni yaratamiz
+const popularProducts = ref<IProduct[]>([]);
+const route = useRoute()
+
+// Sahifa yuklanganda yoki yangilanganda array ni tasodifiy tartiblash
+onBeforeMount(() => {
+    popularProducts.value = shuffleArray([...initialProducts.value]);
+    console.log('aaaaa',);
+
+});
+</script>
+
 <template>
-    <div>
-        Category
-    </div>
+    <Container class="my-5">
+        <div class="flex gap-6">
+
+            <ProductFilter @clear="onClear" @apply="fetchFilter" />
+            <ProductGrid :products="popularProducts" />
+        </div>
+    </Container>
 </template>
--- pages/product/[slug].vue
+++ pages/product/[slug].vue
@@ -1,8 +1,59 @@
 <script setup lang="ts">
+import ProductView from '~/components/Card/ProductView.vue';
+import Container from '~/components/Container.vue';
+import image from "@/assets/images/vegon.png"
+import ProductSection from '~/components/Card/ProductSection.vue';
+import image1 from "@/assets/images/image1.png"
+import image2 from "@/assets/images/image2.png"
+import image3 from "@/assets/images/image3.png"
+import image4 from "@/assets/images/image4.png"
+import type { IProduct } from "@/types"
+const saleProducts = ref<IProduct[]>([
+    {
+        id: 1,
+        name: "Raffaello - Праздничный набор конфет (240г)",
+        originalPrice: 500,
+        price: 450,
+        imageUrl: image1,
+        isNew: true,
+    },
+    {
+        id: 2,
+        name: "Французский парфюм - Для женщин (50мл)",
+        originalPrice: 1200,
+        price: 900,
+        imageUrl: image2,
+    },
+    {
+        id: 3,
+        name: "Ювелирное украшение - Серебряное кольцо",
+        originalPrice: 2500,
+        price: 1900,
+        imageUrl: image3,
+    },
+    {
+        id: 4,
+        name: "Набор сладостей - Шоколад и конфеты",
+        originalPrice: 800,
+        price: 600,
+        imageUrl: image4,
+    },
+]);
 </script>
 
 <template>
-    <div>
-        Product
-    </div>
+    <Container>
+        <div class="grid grid-cols-5 gap-6 my-6 ">
+            <div class="col-span-2">
+                <img class="w-full" :src="image" />
+            </div>
+            <ProductView class="col-span-2"
+                title="Bombbar. Вафельный батончик Bombbar с молочно-ореховой пастой со вкусом капучино 32 г"
+                code="BBR1110411" unit="шт." :currentPrice="53.31" :originalPrice="75.31" :isNew="true"
+                @onAddToCart="(quantity: any) => console.log('Added to cart:', quantity)"
+                @onBuyNow="(quantity: any) => console.log('Buy now:', quantity)"
+                @onWishlist="() => console.log('Added to wishlist')" />
+        </div>
+        <ProductSection :title="'Похожие продукты'" :products="saleProducts" class="" />
+    </Container>
 </template>
